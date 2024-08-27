e80a6748871cc5846ce665ba372bbba2c938b1d9
Decta <m.a.baghyani@gmail.com>
2024-08-27 16:17:20 +0330

    version 1

---
+++ python/tas.py
@@ -0,0 +1,19 @@
+num = int(input())
+
+if num == 6 or num == 1:
+    if num == 1:
+        print(6)
+    if num == 6 :
+        print(1)
+
+if num == 4 or num == 3:
+    if num == 4:
+        print(3)
+    if num == 3:
+        print(4)
+
+if num == 2 or num == 5:
+    if num == 2 :
+        print(5)
+    if num == 5 :
+        print(2)
